6bba85d28d8bf6bcce1171483525bf9cde41f437
andrshv <andrey678a@gmail.com>
2026-03-11 17:55:07 +0200

    Добавил поддержку Resilience4j для сервиса электронной почты, включая механизм Circuit Breaker и Retry. Также добавил конфигурацию Prometheus и Grafana для мониторинга сервиса. Но нужно реализвать стресс тесты через Grafana K6

--- src/main/java/com/example/project/interfaces/EmailService.java
+++ src/main/java/com/example/project/interfaces/EmailService.java
@@ -2,4 +2,5 @@ package com.example.project.interfaces;
 
 public interface EmailService {
     void sendPasswordResetCode(String to, String code);
+    void emailFallback(String to, String code, Throwable ex);
 }
--- src/main/java/com/example/project/service/EmailServiceImpl.java
+++ src/main/java/com/example/project/service/EmailServiceImpl.java
@@ -1,9 +1,10 @@
 package com.example.project.service;
 
-
 import com.example.project.exceptions.EmailSendingExeption;
 import com.example.project.interfaces.EmailService;
 import com.example.project.metrics.EmailMetricsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
@@ -19,7 +20,10 @@ public class EmailServiceImpl implements EmailService {
     private final EmailMetricsService emailMetrics;
 
     @Override
+    @CircuitBreaker(name = "emailService", fallbackMethod = "emailFallback")
+    @Retry(name = "emailService")
     public void sendPasswordResetCode(String to, String code) {
+
         log.info("Отправка письма сброса пароля на: {}", to);
 
         SimpleMailMessage message = new SimpleMailMessage();
@@ -27,15 +31,20 @@ public class EmailServiceImpl implements EmailService {
         message.setSubject("Password reset");
         message.setText("Ваш код: " + code);
 
-        try {
-            log.info("Отправляем письмо на: {}", to);
-            mailSender.send(message);
-            emailMetrics.sent();
-            log.info("Письмо успешно отправлено на: {}", to);
-        } catch (Exception e) {
-            emailMetrics.failed();
-            log.error("Ошибка отправки письма на {}: {}", to, e.getMessage());
-            throw new EmailSendingExeption("Email sending failed", e);
-        }
+        mailSender.send(message);
+
+        emailMetrics.sent();
+
+        log.info("Письмо успешно отправлено на: {}", to);
+    }
+
+    @Override
+    public void emailFallback(String to, String code, Throwable ex) {
+
+        emailMetrics.failed();
+
+        log.error("Email fallback activated for {} : {}", to, ex.getMessage());
+
+        throw new EmailSendingExeption("Email service unavailable", ex);
     }
 }
